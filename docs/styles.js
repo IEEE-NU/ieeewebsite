1dba52364a1fba40433d8ab64d28978bdee545a4
Andy Xu <andyxu12@gmail.com>
2020-06-21 20:39:13 -0400

    Updated exec board and explore pages

---
+++ docs/styles.js
@@ -0,0 +1,548 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["styles"],{
+
+/***/ "./node_modules/raw-loader/index.js!./node_modules/postcss-loader/lib/index.js??embedded!./src/styles.css":
+/*!**********************************************************************************************!*\
+  !*** ./node_modules/raw-loader!./node_modules/postcss-loader/lib??embedded!./src/styles.css ***!
+  \**********************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\r\n\r\n@import url('https://fonts.googleapis.com/css?family=Nunito+Sans:300,500,700|Karla:400,700');\n/* You can add global styles to this file, and also import other style files */\nbody {\r\n    overflow-x: hidden;\r\n    background-color: #eeeeee;\r\n    font-family: 'Nunito Sans', sans-serif;\r\n}\n/**************\r\n    \r\n    TYPOGRAPHY\r\n\r\n**************/\nh1, h2 {\r\n    font-family: 'Nunito Sans', sans-serif;\r\n    font-weight: 700;\r\n    text-align: center;\r\n}\nh2 {\r\n    color: rgb(078, 042, 132);\r\n}\nh2:after {\r\n    content: \"\";\r\n    /* This is necessary for the pseudo element to work. */\r\n    display: block;\r\n    /* This will put the pseudo element on its own line. */\r\n    margin: 0 auto;\r\n    /* This will center the border. */\r\n    width: 5rem;\r\n    /* Change this to whatever width you want. */\r\n    padding-top: .75rem;\r\n    /* This creates some space between the element and the border. */\r\n    border-bottom: .1rem solid rgb(078, 042, 132);\r\n    /* This creates the border. Replace black with whatever color you want. */\r\n    margin-bottom: 2rem;\r\n}\n#tagline:after {\r\n    border-bottom: #eeeeee solid .1rem;\r\n}\n#abt-title {\r\n    text-align: left;\r\n}\n#abt-title:after {\r\n    margin: 0;\r\n}\na, p, li, h3, h4, h5 {\r\n    font-family: 'Karla', sans-serif;\r\n}\nh3 {\r\n    font-weight: 400;\r\n}\na, p, li {\r\n    font-weight: 400;\r\n}\na, a:hover {\r\n    color: rgb(078, 042, 132);\r\n}\np {\r\n    line-height: 1.75rem;\r\n    text-align: left;\r\n}\nsup {\r\n    left: -.2rem;\r\n}\n/**************\r\n   \r\n    BOOTSTRAP MODS\r\n\r\n**************/\n.container {\r\n    margin-bottom: 5rem;\r\n}\n.col-md-4 {\r\n    margin-bottom: 4rem;\r\n}\n/* Carousel base class */\n.carousel {\r\n    margin-bottom: 4rem;\r\n}\n/* Declare heights because of positioning of img element */\n.carousel-item {\r\n    height: 32rem;\r\n    background-color: rgba(40, 40, 40, 0.70);\r\n}\n.carousel-item>img {\r\n    top: 0;\r\n    left: 0;\r\n    display: block;\r\n    margin-left: auto;\r\n    margin-right: auto;\r\n    min-width: 80%;\r\n    height: 32rem;\r\n}\n/**************\r\n   \r\n    HEADSHOTS\r\n\r\n**************/\n.mem img {\r\n    width: 60%;\r\n    border-radius: 50%;\r\n}\n.mem {\r\n    text-align: center;\r\n}\n.mem p {\r\n    text-align: center;\r\n}\n.members {\r\n    text-align: center;\r\n}\n.mem-position {\r\n    margin-top: -.25rem;\r\n    margin-bottom: 1rem;\r\n}\n.mem-name {\r\n    margin-top: .75rem;\r\n}"
+
+/***/ }),
+
+/***/ "./node_modules/style-loader/lib/addStyles.js":
+/*!****************************************************!*\
+  !*** ./node_modules/style-loader/lib/addStyles.js ***!
+  \****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+
+var stylesInDom = {};
+
+var	memoize = function (fn) {
+	var memo;
+
+	return function () {
+		if (typeof memo === "undefined") memo = fn.apply(this, arguments);
+		return memo;
+	};
+};
+
+var isOldIE = memoize(function () {
+	// Test for IE <= 9 as proposed by Browserhacks
+	// @see http://browserhacks.com/#hack-e71d8692f65334173fee715c222cb805
+	// Tests for existence of standard globals is to allow style-loader
+	// to operate correctly into non-standard environments
+	// @see https://github.com/webpack-contrib/style-loader/issues/177
+	return window && document && document.all && !window.atob;
+});
+
+var getTarget = function (target) {
+  return document.querySelector(target);
+};
+
+var getElement = (function (fn) {
+	var memo = {};
+
+	return function(target) {
+                // If passing function in options, then use it for resolve "head" element.
+                // Useful for Shadow Root style i.e
+                // {
+                //   insertInto: function () { return document.querySelector("#foo").shadowRoot }
+                // }
+                if (typeof target === 'function') {
+                        return target();
+                }
+                if (typeof memo[target] === "undefined") {
+			var styleTarget = getTarget.call(this, target);
+			// Special case to return head of iframe instead of iframe itself
+			if (window.HTMLIFrameElement && styleTarget instanceof window.HTMLIFrameElement) {
+				try {
+					// This will throw an exception if access to iframe is blocked
+					// due to cross-origin restrictions
+					styleTarget = styleTarget.contentDocument.head;
+				} catch(e) {
+					styleTarget = null;
+				}
+			}
+			memo[target] = styleTarget;
+		}
+		return memo[target]
+	};
+})();
+
+var singleton = null;
+var	singletonCounter = 0;
+var	stylesInsertedAtTop = [];
+
+var	fixUrls = __webpack_require__(/*! ./urls */ "./node_modules/style-loader/lib/urls.js");
+
+module.exports = function(list, options) {
+	if (typeof DEBUG !== "undefined" && DEBUG) {
+		if (typeof document !== "object") throw new Error("The style-loader cannot be used in a non-browser environment");
+	}
+
+	options = options || {};
+
+	options.attrs = typeof options.attrs === "object" ? options.attrs : {};
+
+	// Force single-tag solution on IE6-9, which has a hard limit on the # of <style>
+	// tags it will allow on a page
+	if (!options.singleton && typeof options.singleton !== "boolean") options.singleton = isOldIE();
+
+	// By default, add <style> tags to the <head> element
+        if (!options.insertInto) options.insertInto = "head";
+
+	// By default, add <style> tags to the bottom of the target
+	if (!options.insertAt) options.insertAt = "bottom";
+
+	var styles = listToStyles(list, options);
+
+	addStylesToDom(styles, options);
+
+	return function update (newList) {
+		var mayRemove = [];
+
+		for (var i = 0; i < styles.length; i++) {
+			var item = styles[i];
+			var domStyle = stylesInDom[item.id];
+
+			domStyle.refs--;
+			mayRemove.push(domStyle);
+		}
+
+		if(newList) {
+			var newStyles = listToStyles(newList, options);
+			addStylesToDom(newStyles, options);
+		}
+
+		for (var i = 0; i < mayRemove.length; i++) {
+			var domStyle = mayRemove[i];
+
+			if(domStyle.refs === 0) {
+				for (var j = 0; j < domStyle.parts.length; j++) domStyle.parts[j]();
+
+				delete stylesInDom[domStyle.id];
+			}
+		}
+	};
+};
+
+function addStylesToDom (styles, options) {
+	for (var i = 0; i < styles.length; i++) {
+		var item = styles[i];
+		var domStyle = stylesInDom[item.id];
+
+		if(domStyle) {
+			domStyle.refs++;
+
+			for(var j = 0; j < domStyle.parts.length; j++) {
+				domStyle.parts[j](item.parts[j]);
+			}
+
+			for(; j < item.parts.length; j++) {
+				domStyle.parts.push(addStyle(item.parts[j], options));
+			}
+		} else {
+			var parts = [];
+
+			for(var j = 0; j < item.parts.length; j++) {
+				parts.push(addStyle(item.parts[j], options));
+			}
+
+			stylesInDom[item.id] = {id: item.id, refs: 1, parts: parts};
+		}
+	}
+}
+
+function listToStyles (list, options) {
+	var styles = [];
+	var newStyles = {};
+
+	for (var i = 0; i < list.length; i++) {
+		var item = list[i];
+		var id = options.base ? item[0] + options.base : item[0];
+		var css = item[1];
+		var media = item[2];
+		var sourceMap = item[3];
+		var part = {css: css, media: media, sourceMap: sourceMap};
+
+		if(!newStyles[id]) styles.push(newStyles[id] = {id: id, parts: [part]});
+		else newStyles[id].parts.push(part);
+	}
+
+	return styles;
+}
+
+function insertStyleElement (options, style) {
+	var target = getElement(options.insertInto)
+
+	if (!target) {
+		throw new Error("Couldn't find a style target. This probably means that the value for the 'insertInto' parameter is invalid.");
+	}
+
+	var lastStyleElementInsertedAtTop = stylesInsertedAtTop[stylesInsertedAtTop.length - 1];
+
+	if (options.insertAt === "top") {
+		if (!lastStyleElementInsertedAtTop) {
+			target.insertBefore(style, target.firstChild);
+		} else if (lastStyleElementInsertedAtTop.nextSibling) {
+			target.insertBefore(style, lastStyleElementInsertedAtTop.nextSibling);
+		} else {
+			target.appendChild(style);
+		}
+		stylesInsertedAtTop.push(style);
+	} else if (options.insertAt === "bottom") {
+		target.appendChild(style);
+	} else if (typeof options.insertAt === "object" && options.insertAt.before) {
+		var nextSibling = getElement(options.insertInto + " " + options.insertAt.before);
+		target.insertBefore(style, nextSibling);
+	} else {
+		throw new Error("[Style Loader]\n\n Invalid value for parameter 'insertAt' ('options.insertAt') found.\n Must be 'top', 'bottom', or Object.\n (https://github.com/webpack-contrib/style-loader#insertat)\n");
+	}
+}
+
+function removeStyleElement (style) {
+	if (style.parentNode === null) return false;
+	style.parentNode.removeChild(style);
+
+	var idx = stylesInsertedAtTop.indexOf(style);
+	if(idx >= 0) {
+		stylesInsertedAtTop.splice(idx, 1);
+	}
+}
+
+function createStyleElement (options) {
+	var style = document.createElement("style");
+
+	if(options.attrs.type === undefined) {
+		options.attrs.type = "text/css";
+	}
+
+	addAttrs(style, options.attrs);
+	insertStyleElement(options, style);
+
+	return style;
+}
+
+function createLinkElement (options) {
+	var link = document.createElement("link");
+
+	if(options.attrs.type === undefined) {
+		options.attrs.type = "text/css";
+	}
+	options.attrs.rel = "stylesheet";
+
+	addAttrs(link, options.attrs);
+	insertStyleElement(options, link);
+
+	return link;
+}
+
+function addAttrs (el, attrs) {
+	Object.keys(attrs).forEach(function (key) {
+		el.setAttribute(key, attrs[key]);
+	});
+}
+
+function addStyle (obj, options) {
+	var style, update, remove, result;
+
+	// If a transform function was defined, run it on the css
+	if (options.transform && obj.css) {
+	    result = options.transform(obj.css);
+
+	    if (result) {
+	    	// If transform returns a value, use that instead of the original css.
+	    	// This allows running runtime transformations on the css.
+	    	obj.css = result;
+	    } else {
+	    	// If the transform function returns a falsy value, don't add this css.
+	    	// This allows conditional loading of css
+	    	return function() {
+	    		// noop
+	    	};
+	    }
+	}
+
+	if (options.singleton) {
+		var styleIndex = singletonCounter++;
+
+		style = singleton || (singleton = createStyleElement(options));
+
+		update = applyToSingletonTag.bind(null, style, styleIndex, false);
+		remove = applyToSingletonTag.bind(null, style, styleIndex, true);
+
+	} else if (
+		obj.sourceMap &&
+		typeof URL === "function" &&
+		typeof URL.createObjectURL === "function" &&
+		typeof URL.revokeObjectURL === "function" &&
+		typeof Blob === "function" &&
+		typeof btoa === "function"
+	) {
+		style = createLinkElement(options);
+		update = updateLink.bind(null, style, options);
+		remove = function () {
+			removeStyleElement(style);
+
+			if(style.href) URL.revokeObjectURL(style.href);
+		};
+	} else {
+		style = createStyleElement(options);
+		update = applyToTag.bind(null, style);
+		remove = function () {
+			removeStyleElement(style);
+		};
+	}
+
+	update(obj);
+
+	return function updateStyle (newObj) {
+		if (newObj) {
+			if (
+				newObj.css === obj.css &&
+				newObj.media === obj.media &&
+				newObj.sourceMap === obj.sourceMap
+			) {
+				return;
+			}
+
+			update(obj = newObj);
+		} else {
+			remove();
+		}
+	};
+}
+
+var replaceText = (function () {
+	var textStore = [];
+
+	return function (index, replacement) {
+		textStore[index] = replacement;
+
+		return textStore.filter(Boolean).join('\n');
+	};
+})();
+
+function applyToSingletonTag (style, index, remove, obj) {
+	var css = remove ? "" : obj.css;
+
+	if (style.styleSheet) {
+		style.styleSheet.cssText = replaceText(index, css);
+	} else {
+		var cssNode = document.createTextNode(css);
+		var childNodes = style.childNodes;
+
+		if (childNodes[index]) style.removeChild(childNodes[index]);
+
+		if (childNodes.length) {
+			style.insertBefore(cssNode, childNodes[index]);
+		} else {
+			style.appendChild(cssNode);
+		}
+	}
+}
+
+function applyToTag (style, obj) {
+	var css = obj.css;
+	var media = obj.media;
+
+	if(media) {
+		style.setAttribute("media", media)
+	}
+
+	if(style.styleSheet) {
+		style.styleSheet.cssText = css;
+	} else {
+		while(style.firstChild) {
+			style.removeChild(style.firstChild);
+		}
+
+		style.appendChild(document.createTextNode(css));
+	}
+}
+
+function updateLink (link, options, obj) {
+	var css = obj.css;
+	var sourceMap = obj.sourceMap;
+
+	/*
+		If convertToAbsoluteUrls isn't defined, but sourcemaps are enabled
+		and there is no publicPath defined then lets turn convertToAbsoluteUrls
+		on by default.  Otherwise default to the convertToAbsoluteUrls option
+		directly
+	*/
+	var autoFixUrls = options.convertToAbsoluteUrls === undefined && sourceMap;
+
+	if (options.convertToAbsoluteUrls || autoFixUrls) {
+		css = fixUrls(css);
+	}
+
+	if (sourceMap) {
+		// http://stackoverflow.com/a/26603875
+		css += "\n/*# sourceMappingURL=data:application/json;base64," + btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap)))) + " */";
+	}
+
+	var blob = new Blob([css], { type: "text/css" });
+
+	var oldSrc = link.href;
+
+	link.href = URL.createObjectURL(blob);
+
+	if(oldSrc) URL.revokeObjectURL(oldSrc);
+}
+
+
+/***/ }),
+
+/***/ "./node_modules/style-loader/lib/urls.js":
+/*!***********************************************!*\
+  !*** ./node_modules/style-loader/lib/urls.js ***!
+  \***********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+
+/**
+ * When source maps are enabled, `style-loader` uses a link element with a data-uri to
+ * embed the css on the page. This breaks all relative urls because now they are relative to a
+ * bundle instead of the current page.
+ *
+ * One solution is to only use full urls, but that may be impossible.
+ *
+ * Instead, this function "fixes" the relative urls to be absolute according to the current page location.
+ *
+ * A rudimentary test suite is located at `test/fixUrls.js` and can be run via the `npm test` command.
+ *
+ */
+
+module.exports = function (css) {
+  // get current location
+  var location = typeof window !== "undefined" && window.location;
+
+  if (!location) {
+    throw new Error("fixUrls requires window.location");
+  }
+
+	// blank or null?
+	if (!css || typeof css !== "string") {
+	  return css;
+  }
+
+  var baseUrl = location.protocol + "//" + location.host;
+  var currentDir = baseUrl + location.pathname.replace(/\/[^\/]*$/, "/");
+
+	// convert each url(...)
+	/*
+	This regular expression is just a way to recursively match brackets within
+	a string.
+
+	 /url\s*\(  = Match on the word "url" with any whitespace after it and then a parens
+	   (  = Start a capturing group
+	     (?:  = Start a non-capturing group
+	         [^)(]  = Match anything that isn't a parentheses
+	         |  = OR
+	         \(  = Match a start parentheses
+	             (?:  = Start another non-capturing groups
+	                 [^)(]+  = Match anything that isn't a parentheses
+	                 |  = OR
+	                 \(  = Match a start parentheses
+	                     [^)(]*  = Match anything that isn't a parentheses
+	                 \)  = Match a end parentheses
+	             )  = End Group
+              *\) = Match anything and then a close parens
+          )  = Close non-capturing group
+          *  = Match anything
+       )  = Close capturing group
+	 \)  = Match a close parens
+
+	 /gi  = Get all matches, not the first.  Be case insensitive.
+	 */
+	var fixedCss = css.replace(/url\s*\(((?:[^)(]|\((?:[^)(]+|\([^)(]*\))*\))*)\)/gi, function(fullMatch, origUrl) {
+		// strip quotes (if they exist)
+		var unquotedOrigUrl = origUrl
+			.trim()
+			.replace(/^"(.*)"$/, function(o, $1){ return $1; })
+			.replace(/^'(.*)'$/, function(o, $1){ return $1; });
+
+		// already a full url? no change
+		if (/^(#|data:|http:\/\/|https:\/\/|file:\/\/\/|\s*$)/i.test(unquotedOrigUrl)) {
+		  return fullMatch;
+		}
+
+		// convert the url to a full url
+		var newUrl;
+
+		if (unquotedOrigUrl.indexOf("//") === 0) {
+		  	//TODO: should we add protocol?
+			newUrl = unquotedOrigUrl;
+		} else if (unquotedOrigUrl.indexOf("/") === 0) {
+			// path should be relative to the base url
+			newUrl = baseUrl + unquotedOrigUrl; // already starts with '/'
+		} else {
+			// path should be relative to current directory
+			newUrl = currentDir + unquotedOrigUrl.replace(/^\.\//, ""); // Strip leading './'
+		}
+
+		// send back the fixed url(...)
+		return "url(" + JSON.stringify(newUrl) + ")";
+	});
+
+	// send back the fixed css
+	return fixedCss;
+};
+
+
+/***/ }),
+
+/***/ "./src/styles.css":
+/*!************************!*\
+  !*** ./src/styles.css ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+var content = __webpack_require__(/*! !../node_modules/raw-loader!../node_modules/postcss-loader/lib??embedded!./styles.css */ "./node_modules/raw-loader/index.js!./node_modules/postcss-loader/lib/index.js??embedded!./src/styles.css");
+
+if(typeof content === 'string') content = [[module.i, content, '']];
+
+var transform;
+var insertInto;
+
+
+
+var options = {"hmr":true}
+
+options.transform = transform
+options.insertInto = undefined;
+
+var update = __webpack_require__(/*! ../node_modules/style-loader/lib/addStyles.js */ "./node_modules/style-loader/lib/addStyles.js")(content, options);
+
+if(content.locals) module.exports = content.locals;
+
+if(false) {}
+
+/***/ }),
+
+/***/ 2:
+/*!******************************!*\
+  !*** multi ./src/styles.css ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! C:\Users\andyx\Desktop\ieeewebsite\src\styles.css */"./src/styles.css");
+
+
+/***/ })
+
+},[[2,"runtime"]]]);
+//# sourceMappingURL=styles.js.map
--- src/app/headshot-control/headshots.ts
+++ src/app/headshot-control/headshots.ts
@@ -1,82 +1,88 @@
-import { headshotItem } from './headshotItem';
+import { headshotItem } from "./headshotItem";
 
 export const headshotItems: headshotItem[] = [
-    // Shankar
-    {
-        img: 'assets/headshots/Shankar.JPG',
-        name: 'Shankar Salwan',
-        position: 'President',
-        alt: 'Pictured: Shankar Salwan, President',
-    },
-    // Sanfeng
-    {
-        img: 'assets/headshots/sanfeng.jpg',
-        name: 'Sanfeng Wang',
-        position: 'Vice President',
-        alt: 'Pictured: Sanfeng Wang, Vice President',
-    },
-    // Kevin
-    {
-        img: 'assets/headshots/kevin_lee.jpg',
-        name: 'Kevin Lee',
-        position: 'IE3 Technical Program Director & Hack Night Coordinator',
-        alt: 'Pictured: Kevin Lee, IE3 Technical Program Director & Hack Night Coordinator',
-    },
-    // Kendall
-    {
-        img: 'assets/headshots/kendall.jpg',
-        name: 'Kendall Kuzminskas',
-        position: 'Treasurer',
-        alt: 'Pictured: Kendall Kuzminskas, Treasurer',
-    },
-    // David
-    {
-        img: 'assets/headshots/david.jpg',
-        name: 'David Ding',
-        position: 'Secretary',
-        alt: 'Pictured: David Ding, Secretary',
-    },
-    // Tyler
-    {
-        img: 'assets/headshots/tyler.jpg',
-        name: 'Tyler Rodgers',
-        position: 'Webmaster',
-        alt: 'Pictured: Tyler Rodgers, Webmaster',
-    },
-    // Aaron
-    {
-        img: 'assets/headshots/aaron.JPG',
-        name: 'Aaron Kaneti',
-        position: 'Social Director',
-        alt: 'Pictured: Aaron Kaneti, Social Director',
-    },
-    // Kimberly
-    {
-        img: 'assets/headshots/kim.jpg',
-        name: 'Kimberly Kwon',
-        position: 'Co-Publicity Chair',
-        alt: 'Pictured: Kimberly Kwon, Co-Publicity Chair',
-    },
-    // Thomas
-    {
-        img: 'assets/headshots/tommy.jpg',
-        name: 'Thomas Li',
-        position: 'Co-Publicity Chair',
-        alt: 'Pictured: Thomas Li, Co-Publicity Chair',
-    },
-    // Conway
-    {
-        img: 'assets/headshots/Conway.JPG',
-        name: 'Conway Zhu',
-        position: 'Legacy Adviser',
-        alt: 'Pictured: Conway Zhu, Legacy Adviser',
-    },
-    // Curtis
-    {
-        img: 'assets/headshots/curtis.jpg',
-        name: 'Curtis Wang',
-        position: 'Graduate Adviser',
-        alt: 'Pictured: Curtis Wang, Graduate Adviser',
-    },
-
+  // Karan
+  {
+    img: "assets/headshots/karan.jpg",
+    name: "Karan Bhasin",
+    position: "President",
+    alt: "Pictured: Karan Bhasin, President",
+  },
+  // Utkarsh
+  {
+    img: "assets/headshots/utkarsh.jpg",
+    name: "Utkarsh Mishra",
+    position: "Vice-President",
+    alt: "Pictured: Utkarsh Mishra, Vice-President",
+  },
+  // Aimee
+  {
+    img: "assets/headshots/aimee.jpg",
+    name: "Aimee van den Berg",
+    position: "Technical Program Director",
+    alt: "Pictured: Aimee van den Berg, Technical Program Director",
+  },
+  // Romir
+  {
+    img: "assets/headshots/romir.jpg",
+    name: "Romir Hysko",
+    position: "Treasurer",
+    alt: "Pictured: Romir Hysko, Treasurer ",
+  },
+  // Sisilia
+  {
+    img: "assets/headshots/sisilia.JPG",
+    name: "Sisilia Sinaga",
+    position: "Secretary",
+    alt: "Pictured: Sisilia Sinaga, Secretary",
+  },
+  // Andy
+  {
+    img: "assets/headshots/andy.jpg",
+    name: "Andy Xu",
+    position: "Webmaster",
+    alt: "Pictured: Andy Xu, Webmaster",
+  },
+  // Evan
+  {
+    img: "assets/headshots/evan.jpg",
+    name: "Evan Costa",
+    position: "Membership Chair",
+    alt: "Pictured: Evan Costa, Membership Chair",
+  },
+  // Jonathan
+  {
+    img: "assets/headshots/jonathan.jpg",
+    name: "Jonathan Liu",
+    position: "Co-Social Chair",
+    alt: "Pictured: Jonathan Liu, Co-Social Chair",
+  },
+  // Vikram
+  {
+    img: "assets/headshots/vikram.JPG",
+    name: "Vikram Thanigaivelan ",
+    position: "Co-Social Chair",
+    alt: "Pictured: Vikram Thanigaivelan , Co-Social Chair",
+  },
+  // Tony
+  {
+    img: "assets/headshots/tony.jpg",
+    name: "Tony Bayvas",
+    position: "Co-Publicity Chair",
+    alt: "Pictured: Tony Bayvas, Co-Publicity Chair",
+  },
+  // Evans
+  {
+    img: "assets/headshots/evans.jpeg",
+    name: "Evans Dingwizat",
+    position: "Co-Publicity Chair",
+    alt: "Pictured: Evans Dingwizat, Co-Publicity Chair",
+  },
+  // Shourya
+  {
+    img: "assets/headshots/shourya.jpg",
+    name: "Shourya Agarwal",
+    position: "Outreach Chair",
+    alt: "Pictured: Shourya Agarwal, Outreach Chair",
+  },
 ];
